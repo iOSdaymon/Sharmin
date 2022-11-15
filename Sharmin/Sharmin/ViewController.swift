52340a666a2943ec2cac15bcc78f59e23dd9c373
daymon <Daymon.vin@gmail.com>
2022-11-15 21:25:43 +0200

    initial commit

---
+++ Sharmin/Sharmin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sharmin
+//
+//  Created by Dima Sviderskyi on 15.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
