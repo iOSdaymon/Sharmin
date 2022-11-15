106ba27073f68adf9a07855f3aab376dbba4062c
daymon <Daymon.vin@gmail.com>
2022-11-15 21:59:11 +0200

    base configurations

---
+++ Sharmin/Sharmin/Modules/UsersList/View/UsersListController.swift
@@ -0,0 +1,13 @@
+//
+//  UsersListController.swift
+//  Sharmin
+//
+//  Created by Dima Sviderskyi on 15.11.2022.
+//
+
+import UIKit
+
+final class UsersListController: UIViewController {
+    
+    
+}
---
+++ Sharmin/Sharmin/SupportingFiles/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Sharmin
+//
+//  Created by Dima Sviderskyi on 15.11.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow()
+        let navigationController = UINavigationController(rootViewController: UsersListController())
+        navigationController.isNavigationBarHidden = true
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
+
