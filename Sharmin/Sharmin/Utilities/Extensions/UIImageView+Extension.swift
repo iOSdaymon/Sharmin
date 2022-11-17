3c98ca02f86f4b205102a6271d718450612c32cb
daymon <Daymon.vin@gmail.com>
2022-11-17 21:03:38 +0200

    updated image downloading process

--- Sharmin/Sharmin/Utilities/Extensions/UIImageView+Extension.swift
+++ Sharmin/Sharmin/Utilities/Extensions/UIImageView+Extension.swift
@@ -12,12 +12,11 @@ var imageCache = NSCache<AnyObject, AnyObject>()
 extension UIImageView {
     
     func set(_ pictureUri: String?) {
-        if let url = pictureUri {
-            load(urlString: url, completion: { [weak self] urlString, image in
-                guard url == urlString else { return }
-                self?.image = image
-            })
-        }
+        guard let url = pictureUri else { return }
+        load(urlString: url, completion: { [weak self] urlString, newImage in
+            guard url == urlString else { return }
+            self?.image = newImage
+        })
     }
     
     func load(urlString: String, completion: @escaping (_ urlString: String, _ image: UIImage) -> ()) {
@@ -31,18 +30,18 @@ extension UIImageView {
         guard let url = URL(string: urlString) else { return }
         
         DispatchQueue.global().async {
-            do {
-                let data = try Data(contentsOf: url)
-                if let image = UIImage(data: data) {
-                    DispatchQueue.main.async {
-                        imageCache.setObject(image, forKey: urlString as NSString)
-                        completion(urlString, image)
-                    }
+            // Create Data Task
+            let dataTask = URLSession.shared.dataTask(with: url) { (data, _, _) in
+                guard let imageData = data,
+                      let image = UIImage(data: imageData) else { return }
+                DispatchQueue.main.async {
+                    imageCache.setObject(image, forKey: urlString as NSString)
+                    completion(urlString, image)
                 }
-            } catch (let error) {
-                debugPrint(error.localizedDescription)
             }
             
+            // Start Data Task
+            dataTask.resume()
         }
     }
     
